34dc0b04897d5c70471d0e32191bc54ece720442
bpamos <bpamos21@gmail.com>
2024-02-09 10:02:53 -0700

    broke out into module

---
+++ modules/node-config-java/outputs.tf
@@ -0,0 +1,8 @@
+
+
+
+# output "memtier-ansible-playbook-cmd" {
+#   description = "java ansible-playbook cmd"
+#   value = format("ansible-playbook %s/ansible//%s_memtier_playbook.yaml --private-key %s -i /tmp/%s_test_node_%s.ini",path.module, var.vpc_name, var.ssh_key_path, var.vpc_name, 0)
+
+# }
